ea83dae0d438f1c7594ebb845a276241b249c42d
Yang Yu <yuyangtj@gmail.com>
2019-01-19 06:47:36 +0100

    Update masrate.py

--- masrate.py
+++ masrate.py
@@ -1,19 +1,31 @@
 #written by YY, 20170408
-#record the MAS rate on Bruker AVANCE III and store the data into file
-#Compatible with Topspin 3.2, which has a Jython compiler
-#the time separation between consecutive measurements is
+#record the MAS rate on Bruker AVAMNCE 3 and store the data into file
+#the time separation between consecutive measurements is 
 #controlled by SLEEP(unit s) and the number of points is determined by
-#for loop.
+#for loop
 
-#MASRGET will get the MAS rate from MAS II unit and store it
+#MASRGET will get the MAS rate for MAS II unit and store it 
 #as a state parameter
+
 import time
-date=time.strftime(“_%d_%b_%Y”, time.localtime(time.time()))
-f=open(“/directory/file”+date,’w’)
-for i in range(100):
-    ct = XCMD(“MASRGET”)
-    masrate=GETPARSTAT(“MASR”)
-    curr_time=time.strftime(“%a, %d %b %Y, %H:%M:%S”, time.localtime(time.time()))
-    f.write(“time:”+curr_time+” ,speed:”+masrate+”\n”)
-    SLEEP(5)
+import os,sys
+
+if len(sys.argv)==1:
+	sleep_time=5
+	num_of_log=50000
+else:
+	sleep_time=float(sys.argv[1])
+	num_of_log=int(sys.argv[2])
+	
+	
+date=time.strftime('_%d_%b_%Y', time.localtime(time.time()))
+
+f=open(('/home/%s/masrate/masrate')%os.getenv('USER')+date,'a')
+for i in range(num_of_log):
+    ct = XCMD('MASRGET')
+    masrate=GETPARSTAT('MASR')
+    curr_time=time.strftime('%a, %d %b %Y, %H:%M:%S', time.localtime(time.time()))
+    f.write(('time: %s speed: %s')% (curr_time, masrate) + "\n")
+    SLEEP(sleep_time)    
 f.close()
+
